e192384bd1cfde50b7490a81525efe0268440a23
cesurkedi <cesurkedixx@protonmail.com>
2024-02-22 22:20:33 +0300

    first

--- tools/model.py
+++ tools/model.py
@@ -1,4 +1,5 @@
 import sqlite3
+import streamlit as st
 
 class Database:
     """
@@ -9,6 +10,27 @@ class Database:
 
     def __init__(self, database_name):
         self.database_name = f"{database_name}.sqlite3"
+
+    def createTable(self, table_name, table_columns):
+        """
+        Create a table
+
+        Args:
+            table_columns (dict) : For column of table - dict must have column name in keys, data type in values
+
+        Returns:
+            It only created a table in the database
+        """
+
+        conn = sqlite3.connect(self.database_name)
+        c = conn.cursor()
+        values = ""
+        for k, v in table_columns.items():
+            values += f"{k} {v}, "
+        values = values[:-2]
+        sql_command = f"CREATE TABLE IF NOT EXISTS {table_name} ({values})"
+        c.execute(sql_command)
+        conn.commit()
 
     def createTable(self, table_name, table_columns):
         """
@@ -43,11 +65,29 @@ class Database:
         """
         conn = sqlite3.connect(self.database_name)
         c = conn.cursor()
+        mock_values = ["?"] * len(datas)
+        mock_values = ",".join(mock_values)
+        sql_command = f"INSERT INTO {table_name} VALUES ({mock_values})"
+        c.execute(sql_command, datas)
+        conn.commit()
+    def insertTableMany(self, table_name, datas):
+        """
+        Insert datas to table
+
+        Args:
+            datas (list) : For column of table - dict must have column name in keys, data type in values
+
+        Returns:
+            It only inserted datas to the table
+        """
+        conn = sqlite3.connect(self.database_name)
+        c = conn.cursor()
         mock_values = ["?"] * len(datas[0])
         mock_values = ",".join(mock_values)
         sql_command = f"INSERT INTO {table_name} VALUES ({mock_values})"
         c.executemany(sql_command, datas)
         conn.commit()
+
 
     def getTable(self, table_name):
         conn = sqlite3.connect(self.database_name)
@@ -55,6 +95,14 @@ class Database:
         sql_command = f"SELECT * FROM {table_name}"
         c.execute(sql_command)
         return c.fetchall()
+
+    def getTableSortByColumnName(self, table_name,columnName):
+        conn = sqlite3.connect(self.database_name)
+        c = conn.cursor()
+        sql_command = f"SELECT * FROM {table_name} ORDER BY {columnName} DESC"
+        c.execute(sql_command)
+        return c.fetchall()
+
 
     def getTableWithColumns(self, table_name, *columns):
         conn = sqlite3.connect(self.database_name)
@@ -74,12 +122,6 @@ class Database:
         return price[0]
 
 
-
-
-
-
-
-
 class Modificators:
 
     def __init__(self, input):
@@ -87,7 +129,8 @@ class Modificators:
 
     def correctionForLocation(self):
         input = self.input.replace("°", ".").replace("′", "").replace("″", "").split(" ")
-        location = (float(input[0]), float(input[3]))
+        # location = (float(input[0]), float(input[3]))
+        location = f"{input[0]}, {input[3]}"
         return location
 
     def changerForTurkishLetter(self):
